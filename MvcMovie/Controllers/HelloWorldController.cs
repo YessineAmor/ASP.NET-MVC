f20d598ecf8ad2d1d25f9647af4ef22233199dac
YessineAmor <yassine.amor@esprit.tn>
2019-04-26 12:34:41 +0100

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(String name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID = " + ID);
+        }
+
+        [Route("WelcomeYou/{name}/{numTimes:int}")]
+        public string WelcomeYou(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
